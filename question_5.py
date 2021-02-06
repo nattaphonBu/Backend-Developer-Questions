d0dbb26aef875ece30a3beb87e0479eccd1111cc
unknown <natlove.natlove202@gmail.com>
2021-02-06 20:18:34 +0700

    Question5 : Query for What cars does each person have?

---
+++ question_5.py
@@ -0,0 +1,7 @@
+Question 5
+# SELECT
+# 	first_name as name, 
+# 	car_name as car
+# FROM user_cars
+# INNER JOIN users ON users.user_id = user_cars.user_id 
+# INNER JOIN cars ON cars.car_id  = user_cars.car_id;
